87cc44369005eb4204a85de66d3df1e5c5b166a8
Meha931 <33491127+Meha931@users.noreply.github.com>
2020-04-28 00:20:31 +0700

    Release
    Converted everything to scale, and changed the page a bit.
    it actually works, somehow(?)!

--- protogen/prototest.js
+++ protogen/prototest.js
@@ -1,43 +1,43 @@
 /// The proto code! Do copy this one to draw it
 /// (note it's just 'c', not 'ctx')
-c.clearRect(0,0,canvas.width,canvas.height); // clear stuff, just for redrawing and debugging
+// c.clearRect(0,0,canvas.width,canvas.height); // clear stuff, just for redrawing and debugging
 function render(scale=1) {
 function visor(fill) { // Visor!
 c.beginPath(); // a new starter point, ...for redrawing too, yeah
 c.arc( // so uh, a roundy curve,..
-    50, 50, // at 50;50,..
-    20, // with a small radius
+    (50)*scale, (50)*scale, // at 50;50,..
+    (20)*scale, // with a small radius
     3*Math.PI/2, Math.PI/2, true // what is this? supposed to draw from top to down, by the left side
 ); // ...and that was the back of the head
 //c.lineTo(50+30, 50+20); -- obsolete, too sharp at start
 //c.arcTo(50+60, 50+20, 50, 50-20, 10); c.lineTo(50, 50-20); -- obsolete, too sharp at end
 c.bezierCurveTo( // here goes mess, don't read further it's too weird... (also i don't really know what a bezier curve is)
-    50+80, 50+20+5, // control point one: x+offset (way far to the right for the muzzle); y+radius+offset (a liiitle bit lower than needed),
-    50+20, 50-20, // control point two: x+offset (somewhere between arc center and cp1); y-offset (above center),
-    50, 50-20 // finish this all where it all started: x of center; y of center and [radius] pixels above... nevermind, just where the arc was started
+    (50+80)*scale, (50+20+5)*scale, // control point one: x+offset (way far to the right for the muzzle); y+radius+offset (a liiitle bit lower than needed),
+    (50+20)*scale, (50-20)*scale, // control point two: x+offset (somewhere between arc center and cp1); y-offset (above center),
+    (50)*scale, (50-20)*scale // finish this all where it all started: x of center; y of center and [radius] pixels above... nevermind, just where the arc was started
 ); // kinda fine, already too bored to change, or maybe it's just a bit difficult
 if (fill) c.fill(); else c.stroke(); // to select the drawing style
 }
 function headband(fill) { // idk how it's called
     let points = { // to copy the lines with offset... uh, to "dupe" the line correctly... ahh, just some data
-        x1: 50+5, y1: 50-20+1, // start
-        cpx: 50-10, cpy: 50-6, // quad line
-        x: 50-10, y: 50+18, // ..same quad line
-        offx: -5, offy: -1,
-        radius: 20 // just why ?
+        x1: (50+5)*scale, y1: (50-20+1)*scale, // start
+        cpx: (50-10)*scale, cpy: (50-6)*scale, // quad line
+        x: (50-10)*scale, y: (50+18)*scale, // ..same quad line
+        offx: (-5)*scale, offy: (-1)*scale,
+        radius: (20)*scale // just why ?
     };
     c.beginPath();
     c.moveTo(points["x1"], points["y1"]);
     c.quadraticCurveTo( // uhh..? this should work, i guess... um?
         points["cpx"], points["cpy"],
         points["x"], points["y"],
-        20 // nah, not points["radius"]
+        (20)*scale // nah, not points["radius"]
     );
     c.lineTo(points["x"]+points["offx"], points["y"]+points["offy"]);
     c.quadraticCurveTo(
         points["cpx"]+points["offx"], points["cpy"]+points["offy"],
         points["x1"]+points["offx"], points["y1"]+points["offy"],
-        20
+        (20)*scale
     ); // (yes!! it works!)
     c.closePath(); // easier than c.lineTo(points["x1"], points["y1"]); hehe
     if (fill) c.fill(); else c.stroke();
@@ -49,19 +49,19 @@ function ear(x,y,fill) {
         x: -15, y: 35
     };
     c.beginPath();
-    c.moveTo(x-2.5+2, y+2.5+2);
+    c.moveTo((x-2.5+2)*scale, (y+2.5+2)*scale);
     c.bezierCurveTo(
-        x+points["cp1x"], y+points["cp1y"],
-        x+points["cp2x"], y+points["cp2y"],
-        x+points["x"], y+points["y"]
+        (x+points["cp1x"])*scale, (y+points["cp1y"])*scale,
+        (x+points["cp2x"])*scale, (y+points["cp2y"])*scale,
+        (x+points["x"])*scale, (y+points["y"])*scale
     );
     if (fill) c.fill(); else c.stroke();
 }
 function cheekplate(fill) { // *cheekplates*, hilarious... thb i don't know how are these called
     let points = {
-        x: 50-7.5, y: 50+10, // don't forget to copy to cheekcircle()!
-        radius: 7.5,
-        offx: -30+2.5, stretch: 7.5 // for the bezier curve instead of a quad curve
+        x: (50-7.5)*scale, y: (50+10)*scale, // don't forget to copy to cheekcircle()!
+        radius: (7.5)*scale,
+        offx: (-30+2.5)*scale, stretch: (7.5)*scale // for the bezier curve instead of a quad curve
     };
     c.beginPath();
     c.arc(points["x"], points["y"], points["radius"], Math.PI/2, 3*Math.PI/2, true); // why 'true' though?..
@@ -78,8 +78,8 @@ function cheekplate(fill) { // *cheekplates*, hilarious... thb i don't know how 
 }
 function cheekcircle(fill) { // x3
     let points = {
-        x: 50-7.5, y: 50+10, // copy from cheekplate() here
-        radius: 5
+        x: (50-7.5)*scale, y: (50+10)*scale, // copy from cheekplate() here
+        radius: (5)*scale
     };
     c.beginPath();
     c.arc(points["x"], points["y"], points["radius"], 0, Math.PI*2); // a full circle
@@ -87,8 +87,8 @@ function cheekcircle(fill) { // x3
 }
 function display() {
     let points = {
-        xe: 87.5/*mess*/, ye: 65, // xEnd and yEnding - edge of visor
-        xs: 50+5, ys: 50+5 // xSmile and ySmiling - edge of the smile ^^)
+        xe: (87.5/*mess*/)*scale, ye: (65)*scale, // xEnd and yEnding - edge of visor
+        xs: (50+5)*scale, ys: (50+5)*scale // xSmile and ySmiling - edge of the smile ^^)
     };
     c.beginPath();
     c.moveTo(points["xe"], points["ye"]);
@@ -103,8 +103,8 @@ function display() {
     // And a display...
     c.beginPath();
     (function(x, y, tilt){ // I'm tired of objects, gonna use a function
-        c.arc(x, y, 5, 0+tilt, Math.PI+tilt, true); // And simply arc() for now.. ugh...
-    })(50+10, 50-5, Math.PI/12); // this tilt of PI/12 reminds of some other proto...
+        c.arc(x, y, (5)*scale, 0+tilt, Math.PI+tilt, true); // And simply arc() for now.. ugh...
+    })((50+10)*scale, (50-5)*scale, Math.PI/12); // this tilt of PI/12 reminds of some other proto...
     c.fill();
 }
 
@@ -114,7 +114,7 @@ c.strokeStyle = "#000";
 c.fillStyle = "green";
 ear(50+5, 50-20-1, true);
 ear(50+5, 50-20-1);
-c.fillStyle = "#202050";
+c.fillStyle = "#101040";
 visor(true);
 //c.fillStyle = "deepskyblue";
 //c.strokeStyle = "darkturquoise";
@@ -136,4 +136,8 @@ c.fillStyle = "deepskyblue";
 cheekcircle(true);
 cheekcircle();
 }
-render();
+//render(2); -- YES!!! The end! It somehow works!!
+/*
+    Okay, seriously, I'm fine with current (as of 27.04.2020) result. And it's... already midnight here, i want to sleep ^v^
+    Good luck with everything in your life~.
+*/
